5b702159522efd278d764c365e54f645c44ff49c
rahadi28 <hello.adie@yahoo.com>
2020-01-18 09:53:23 +0700

    Add project PrimeNumber

---
+++ PrimeNumber/src/com/enigmacamp/PrimeNumber.java
@@ -0,0 +1,33 @@
+package com.enigmacamp;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		System.out.print("Input number : ");
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		
+		for (int i = 1; i <= input; i++) {
+			int counter = 0;
+			
+			if (i == 1) {
+				System.out.println(i + " is not prime");
+				continue;
+			} else {
+				for (int j = 1; j <= i; j++) {
+					if (i % j == 0) {
+						counter += 1;
+					}
+				}
+			}
+			
+			if (counter == 2) {
+				System.out.println(i + " is prime");
+			} else {
+				System.out.println(i + " is not prime");
+			}
+		}
+	}
+}
