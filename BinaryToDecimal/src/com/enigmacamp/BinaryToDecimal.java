ad0be2e7cbfa46f4908430a2a586eaa8b9c5cc9c
rahadi28 <hello.adie@yahoo.com>
2020-02-29 09:04:26 +0700

    Add comment message

--- BinaryToDecimal/src/com/enigmacamp/BinaryToDecimal.java
+++ BinaryToDecimal/src/com/enigmacamp/BinaryToDecimal.java
@@ -3,21 +3,21 @@ package com.enigmacamp;
 public class BinaryToDecimal {
 	private static int getDecimal(int binary) {
 		int decimal = 0;
-		int n = 0;
+		int i = 0; //counter index of element binary number
 		while (true) {
 			if (binary == 0) {
 				break;
 			} else {
-				int temp = binary % 10;
-				decimal += temp * Math.pow(2, n);
-				binary = binary / 10;
-				n++;
+				int num = binary % 10; //to get element of binary number 1 or 0
+				decimal += num * Math.pow(2, i);
+				binary = binary / 10; //divide binary number until 0
+				i++;
 			}
 		}
 		return decimal;
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Decimal of 1101110011 is: "+getDecimal(1101110011));
+		System.out.println("Decimal of 1101110011 is: " + getDecimal(1101110011));
 	}
 }
