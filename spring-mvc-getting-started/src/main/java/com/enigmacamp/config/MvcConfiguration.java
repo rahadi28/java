d1fc82042c5d362b2a88273c0911673653e4167e
rahadi28 <hello.adie@yahoo.com>
2020-08-31 16:44:03 +0700

    [UPDATED] Use java version 11 and spring 5

--- spring-mvc-getting-started/src/main/java/com/enigmacamp/config/MvcConfiguration.java
+++ spring-mvc-getting-started/src/main/java/com/enigmacamp/config/MvcConfiguration.java
@@ -6,23 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan(basePackages="com.enigmacamp")
+@ComponentScan(basePackages = "com.enigmacamp")
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter{
+public class MvcConfiguration implements WebMvcConfigurer {
 	@Bean
-	public ViewResolver getViewResolver(){
+	public ViewResolver getViewResolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-	}	
+	}
 }
